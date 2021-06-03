9a9157bf6199a507587bb7b84817b5e72f954a2a
TDSchreur <dennis@tdschreur.nl>
2021-06-03 14:11:18 +0200

    Terraform insights

---
+++ deploy/terraform/modules/insights/main.tf
@@ -0,0 +1,51 @@
+resource "azurerm_application_insights" "insights" {
+  name                = "insights_terraform"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  application_type    = "web"
+}
+
+resource "azurerm_monitor_action_group" "insights" {
+  name                = "Becca alert group"
+  resource_group_name = var.resource_group_name
+  short_name          = "Beccamonitor"
+
+  email_receiver {
+    name          = "Email Dennis"
+    email_address = "t.d.schreur@dnb.nl"
+  }
+
+  email_receiver {
+    name          = "Email Dennis2"
+    email_address = "dennis@tdschreur.nl"
+  }
+}
+
+resource "azurerm_monitor_scheduled_query_rules_alert" "insights" {
+  name                = "Warning with EventId 4001"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  action {
+    action_group = [
+      azurerm_monitor_action_group.insights.id
+    ]
+    email_subject = "Becca warning op production"
+  }
+  data_source_id = azurerm_application_insights.insights.id
+  description    = "Warning with EventId 4001"
+  enabled        = true
+  query          = <<-QUERY
+  traces
+  | where severityLevel == 2
+  | where customDimensions.EventId == 4001
+  | order by timestamp desc
+QUERY
+  severity       = 3
+  frequency      = 5
+  time_window    = 5
+  trigger {
+    operator  = "GreaterThan"
+    threshold = 0
+  }
+}
---
+++ deploy/terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "location" {
+  description = "The Azure location where all resources in this example should be created"
+  default     = "westeurope"
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  default     = "rg_terraform"
+}
+
