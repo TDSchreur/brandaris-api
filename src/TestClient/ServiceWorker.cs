5ca5b4cfd90be9790ea2720555e9094623f41ead
Dennis Schreur <dennis@tdschreur.nl>
2021-09-21 15:22:02 +0200

    small fixes

--- src/TestClient/ServiceWorker.cs
+++ src/TestClient/ServiceWorker.cs
@@ -60,46 +60,46 @@ namespace TestClient
 
         private async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            ////string scope = _configuration["Authentication:Scope"];
-            ////AuthenticationResult authenticationResult = await _tokenHelper.GetTokens(scope);
-            ////_logger.LogInformation("AccessToken: {AccessToken}", authenticationResult.AccessToken);
-
-            ////using HttpClient httpClient = new();
-            ////httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + authenticationResult.AccessToken);
-
-            ////using (HttpRequestMessage request = new(HttpMethod.Get, "https://localhost:5001/api/config"))
-            ////{
-            ////    HttpResponseMessage response = await httpClient.SendAsync(request, stoppingToken);
-            ////    _logger.LogInformation("StatusCode: {StatusCode}", response.StatusCode);
-
-            ////    string data = await response.Content.ReadAsStringAsync(stoppingToken);
-            ////    _logger.LogInformation("Content: {Content}", data);
-            ////}
-
-            ////using (HttpRequestMessage request = new(HttpMethod.Get, "https://localhost:5001/api/person"))
-            ////{
-            ////    HttpResponseMessage response = await httpClient.SendAsync(request, stoppingToken);
-            ////    _logger.LogInformation("StatusCode: {StatusCode}", response.StatusCode);
-
-            ////    string data = await response.Content.ReadAsStringAsync(stoppingToken);
-            ////    _logger.LogInformation("Content: {Content}", data);
-            ////}
-
-            string scope = "https://nta7tp2n6crj4.azurewebsites.net/.default";
+            string scope = _configuration["Authentication:Scope"];
             AuthenticationResult authenticationResult = await _tokenHelper.GetTokens(scope);
-
             _logger.LogInformation("AccessToken: {AccessToken}", authenticationResult.AccessToken);
 
-            using (HttpRequestMessage request = new(HttpMethod.Get, "https://nta7tp2n6crj4.azurewebsites.net/api/GetData"))
-            {
-                request.Headers.Add("Authorization", "Bearer " + authenticationResult.AccessToken);
+            using HttpClient httpClient = new();
+            httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + authenticationResult.AccessToken);
 
-                HttpResponseMessage response = await _httpClient.SendAsync(request, stoppingToken);
+            using (HttpRequestMessage request = new(HttpMethod.Get, "https://localhost:5001/api/config"))
+            {
+                HttpResponseMessage response = await httpClient.SendAsync(request, stoppingToken);
                 _logger.LogInformation("StatusCode: {StatusCode}", response.StatusCode);
 
                 string data = await response.Content.ReadAsStringAsync(stoppingToken);
                 _logger.LogInformation("Content: {Content}", data);
             }
+
+            using (HttpRequestMessage request = new(HttpMethod.Get, "https://localhost:5001/api/person"))
+            {
+                HttpResponseMessage response = await httpClient.SendAsync(request, stoppingToken);
+                _logger.LogInformation("StatusCode: {StatusCode}", response.StatusCode);
+
+                string data = await response.Content.ReadAsStringAsync(stoppingToken);
+                _logger.LogInformation("Content: {Content}", data);
+            }
+
+            ////string scope = "https://nta7tp2n6crj4.azurewebsites.net/.default";
+            ////AuthenticationResult authenticationResult = await _tokenHelper.GetTokens(scope);
+
+            ////_logger.LogInformation("AccessToken: {AccessToken}", authenticationResult.AccessToken);
+
+            ////using (HttpRequestMessage request = new(HttpMethod.Get, "https://nta7tp2n6crj4.azurewebsites.net/api/GetData"))
+            ////{
+            ////    request.Headers.Add("Authorization", "Bearer " + authenticationResult.AccessToken);
+
+            ////    HttpResponseMessage response = await _httpClient.SendAsync(request, stoppingToken);
+            ////    _logger.LogInformation("StatusCode: {StatusCode}", response.StatusCode);
+
+            ////    string data = await response.Content.ReadAsStringAsync(stoppingToken);
+            ////    _logger.LogInformation("Content: {Content}", data);
+            ////}
 
             _hostApplicationLifetime.StopApplication();
         }
