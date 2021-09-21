eb3ecb2f29e9dc3645a197d626b66b1522190278
Dennis Schreur <dennis@tdschreur.nl>
2021-09-21 15:55:23 +0200

    Add ipfilter

---
+++ src/IPFiltering/IpFilterMiddleware.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace IPFiltering
+{
+    public class IpFilterMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<IpFilterMiddleware> _logger;
+        private readonly IEnumerable<IPAddress> _ipAddresses;
+        private readonly IEnumerable<IPNetwork> _ipNetworks;
+
+        public IpFilterMiddleware(RequestDelegate next, ILogger<IpFilterMiddleware> logger, IpSafeList safeList)
+        {
+            _ipAddresses = (!string.IsNullOrWhiteSpace(safeList.IpAddresses) && safeList.IpAddresses.Split(';').Length > 0)
+                ? safeList.IpAddresses.Split(';').Select(IPAddress.Parse).ToList()
+                : Enumerable.Empty<IPAddress>();
+
+            _ipNetworks = (!string.IsNullOrWhiteSpace(safeList.IpNetworks) && safeList.IpNetworks.Split(';').Length > 0)
+                ? safeList.IpNetworks.Split(';').Select(IPNetwork.Parse).ToList()
+                : Enumerable.Empty<IPNetwork>();
+
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            IPAddress remoteIp = context.Connection.RemoteIpAddress ?? throw new ArgumentException("Remote IP is NULL, may due to missing ForwardedHeaders.");
+
+            _logger.LogDebug("Remote IpAddress: {RemoteIp}", remoteIp);
+
+            if (remoteIp.IsIPv4MappedToIPv6)
+            {
+                remoteIp = remoteIp.MapToIPv4();
+            }
+
+            if (!_ipAddresses.Contains(remoteIp) &&
+                !_ipNetworks.Any(x => x.Contains(remoteIp)))
+            {
+                _logger.LogWarning($"Forbidden Request from IP: {remoteIp}");
+                context.Response.StatusCode = StatusCodes.Status403Forbidden;
+                return;
+            }
+
+            await _next.Invoke(context);
+        }
+    }
+}
---
+++ src/IPFiltering/IpSafeList.cs
@@ -0,0 +1,9 @@
+﻿namespace IPFiltering
+{
+    public class IpSafeList
+    {
+        public string IpAddresses { get; set; } = string.Empty;
+
+        public string IpNetworks { get; set; } = string.Empty;
+    }
+}
