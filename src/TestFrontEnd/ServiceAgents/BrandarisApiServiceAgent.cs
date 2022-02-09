d7dc4e9129bd6037593e54f6f92bdf5c5265e426
T.D.Schreur <dennis@tdschreur.nl>
2022-02-09 14:07:45 +0100

    Rename resources

--- src/TestFrontEnd/ServiceAgents/BrandarisApiServiceAgent.cs
+++ src/TestFrontEnd/ServiceAgents/BrandarisApiServiceAgent.cs
@@ -21,19 +21,6 @@ public class BrandarisApiServiceAgent : IBrandarisApiServiceAgent
         _httpClient = httpClient;
     }
 
-    public async Task<GetPersonResponse> GetPersonAsync(int id)
-    {
-        string[] scope =
-        {
-            "api://brandaris-api/manage-data"
-        };
-        string accessToken = await _tokenAcquisition.GetAccessTokenForUserAsync(scope);
-        using HttpRequestMessage request = new(HttpMethod.Get, $"/api/person/{id}");
-        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-        HttpResponseMessage response = await _httpClient.SendAsync(request);
-        return await response.Content.ReadFromJsonAsync<GetPersonResponse>();
-    }
-
     public async Task<IEnumerable<KeyValuePair<string, string>>> GetRemoteClaimsAsync()
     {
         string[] scope =
--- src/TestFrontEnd/ServiceAgents/IBrandarisApiServiceAgent.cs
+++ src/TestFrontEnd/ServiceAgents/IBrandarisApiServiceAgent.cs
@@ -6,7 +6,5 @@ namespace TestFrontEnd.ServiceAgents;
 
 public interface IBrandarisApiServiceAgent
 {
-    Task<GetPersonResponse> GetPersonAsync(int id);
-
     Task<IEnumerable<KeyValuePair<string, string>>> GetRemoteClaimsAsync();
 }
