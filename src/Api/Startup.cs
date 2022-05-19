b0b59ea27a52942ddb617d44bd9ed7196bf6a92e
Dennis Schreur <dennis@tdschreur.nl>
2022-05-19 10:35:24 +0200

    update package, and trace for AI

--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace Brandaris.Api;
 
@@ -22,6 +23,8 @@ public class Startup
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
+        app.UseSerilogRequestLogging();
+
         if (env.IsDevelopment())
         {
             app.UseDeveloperExceptionPage();
