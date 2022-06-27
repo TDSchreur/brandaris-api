6ec1a3140d1db67861c26668418890a2a58255f0
T.D.Schreur <dennis@tdschreur.nl>
2022-06-27 11:42:00 +0200

    Add E2E test, update pipeline

---
+++ tst/IntegrationTests/Drivers/Driver.cs
@@ -0,0 +1,6 @@
+using System;
+
+namespace IntegrationTests.Drivers
+{
+    public class Driver { }
+}
---
+++ tst/IntegrationTests/Hooks/Hook.cs
@@ -0,0 +1,8 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace IntegrationTests.Hooks
+{
+    [Binding]
+    public class Hooks { }
+}
